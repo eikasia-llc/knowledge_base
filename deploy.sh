004e9a4234fda283ae58151aed0ddcb65be862b9
Francisco Zuviría <FranciscodeZuviria@gmail.com>
2026-02-04 22:43:01 -0300

    mejoras deploy

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@
 PROJECT_ID=$(gcloud config get-value project)
 SERVICE_NAME="knowledge-base-app"
 REGION="us-central1"
-IMAGE_NAME="knowledge-base-repo/${PROJECT_ID}/${SERVICE_NAME}"
+IMAGE_NAME="us-central1-docker.pkg.dev/${PROJECT_ID}/knowledge-base-repo/${SERVICE_NAME}"
 
 # Build the Docker image
 echo "Building Docker image: ${IMAGE_NAME}..."
 docker build -t ${IMAGE_NAME} .
 
-# Push to Artifact Registry/GCR
-echo "Pushing image to GCR..."
+# Push to Artifact Registry
+echo "Pushing image to Artifact Registry..."
 docker push ${IMAGE_NAME}
 
 # Deploy to Cloud Run
